dbb9770a810c5b56a65da6bb6846ef3254729996
Daniel Rodilosso <69626728+danielrodilosso@users.noreply.github.com>
2022-03-22 15:31:34 +0100

    Update buttonLogic.cpp

--- src/catch-the-bouncing-led-ball/buttonLogic.cpp
+++ src/catch-the-bouncing-led-ball/buttonLogic.cpp
@@ -1,10 +1,11 @@
 #include <TimerOne.h>
 #include <PinChangeInterrupt.h>
+#include <EnableInterrupt.h>
 
-int btn_0 = 7;
-int btn_1 = 8;
-int btn_2 = 9;
-int btn_3 = 10;
+int btn_0 = 3;
+int btn_1 = 4;
+int btn_2 = 5;
+int btn_3 = 6;
 
 int positionButtonPressed = 0;
 int buttonToPress = 0;
@@ -21,43 +22,57 @@ void setup() {
   pinMode(btn_1, INPUT);
   pinMode(btn_2, INPUT);
   pinMode(btn_3, INPUT);
+
+  //Interrupts Buttons
+  enableInterrupt(btn_0, pressedButton, CHANGE);
+  enableInterrupt(btn_1, pressedButton, CHANGE);
+  enableInterrupt(btn_2, pressedButton, CHANGE);
+  enableInterrupt(btn_3, pressedButton, CHANGE);
+
+  Timer1.initialize(5000000);
+  Timer1.attachInterrupt(checkPositionButton);
+}
+
+void pressedButton() {
+  if (digitalRead(btn_0) == HIGH) {
+    positionButtonPressed = 0;
+  } else if (digitalRead(btn_1) == HIGH) {
+    positionButtonPressed = 1;
+  } else if (digitalRead(btn_2) == HIGH) {
+    positionButtonPressed = 2;
+  } else if (digitalRead(btn_3) == HIGH) {
+    positionButtonPressed = 3;
+  }
+}
+
+void checkPositionButton() {
+   if (positionButtonPressed == buttonToPress) {
+          Serial.println("BRAVO CONTINUA COSI'");
+      } else {
+          Serial.println("GAME-OVER");
+      }
+    generatedLedToPress = false;
 }
 
 void loop() {
+  if (!generatedLedToPress) {
+    Timer1.stop();  
+  } else {
+    Timer1.resume();
+  }
+  
   //Vengono fermati i LED per permettere la pressione
   if (pauseLeds(4000)) {
       if (!generatedLedToPress) {
-        buttonToPress = random(3); 
+        buttonToPress = random(4); 
+        Serial.println("Generato Numero");
         generatedLedToPress = true;
-        //delay(30);
+        Serial.println(buttonToPress);
       }
-            
-      //Serial.println(buttonToPress);
   }
   
-  Serial.println(positionButtonPressed);
-  
-  if (digitalRead(7) == HIGH) {
-    positionButtonPressed = 0;
-  } else if(digitalRead(8) == HIGH) {
-    positionButtonPressed = 1;
-  } else if(digitalRead(9) == HIGH) {
-    positionButtonPressed = 2;
-  } else if(digitalRead(10) == HIGH) {
-    positionButtonPressed = 3;
-  }
-
-  //Pausa di 5s per premere il bottone giusto
-  if (timeToPress(5000)) {
-      if (positionButtonPressed == buttonToPress) {
-          //Serial.println("BRAVO CONTINUA COSI'");
-      } else {
-          //Serial.println("GAME-OVER");
-      }
-      generatedLedToPress = false;
-   }
-
-   delay(300);
+    Serial.println(positionButtonPressed);
+    delay(300);
 }
 
 int pauseLeds(int timerPause) {
@@ -67,29 +82,9 @@ int pauseLeds(int timerPause) {
 
   //scatta l'interrupt
   if (dt >= timerPause) {
-      //Serial.println("PAUSE");
       t1 = millis();
       ret = 1;
       startPause = true;
-  }
-  
-  return ret;
-}
-
-int timeToPress(int tPress) {
-  static unsigned long t1, dt; 
-  int ret = 0;
-
-  if(startPause) {
-    dt = millis() - t1;
-  }
-  
-  //scatta l'interrupt
-  if (dt >= tPress) {
-      //Serial.println("STOP");
-      t1 = millis();
-      ret = 1;
-      startPause = false;
   }
   
   return ret;
